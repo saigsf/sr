bc2bfab200389e82c901b32f60bfafed51b58190
saigsf <2270029397@qq.com>
2018-09-23 19:47:50 +0800

    server

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
+router.get('/getList', function(req, res, next) {
+  res.json({
+    status: '',
+    message: '',
+    data: []
+  });
+});
+/* 修改用户信息. */
+router.get('/update', function(req, res, next) {
+  res.send('respond with a resource');
+});
+/* 删除用户 */
+router.get('/delete', function(req, res, next) {
+  res.send('respond with a resource');
+});
+/* 添加用户. */
+router.get('/add', function(req, res, next) {
   res.send('respond with a resource');
 });
 
