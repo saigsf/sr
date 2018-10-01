aadc815267c43442da506e65a654dc5b36c30ab0
saigsf <2270029397@qq.com>
2018-10-01 09:16:34 +0800

    更新

---
+++ client/src/view/TCU/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="users">
+    <transition name="slide-left" mode="out-in">
+      <keep-alive>
+        <router-view class="child-view"></router-view>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TCU'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/base/variables.scss';
+</style>
